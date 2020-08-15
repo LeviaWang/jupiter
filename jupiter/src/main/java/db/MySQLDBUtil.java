8f0f356934edd34155861814312197c4fbffa558
LeviaWang <654761534@qq.com>
2020-08-15 03:02:36 -0700

    init commit

---
+++ jupiter/src/main/java/db/MySQLDBUtil.java
@@ -0,0 +1,18 @@
+package db;
+
+import java.sql.DriverManager;
+import java.sql.Statement;
+import java.sql.Connection;
+import java.sql.*;
+
+public class MySQLDBUtil {
+	private static final String INSTANCE = "laiproject-instance.ci9hwtaufs7w.us-west-1.rds.amazonaws.com";
+	private static final String PORT_NUM = "3306";
+	public static final String DB_NAME = "laiproject";
+	private static final String USERNAME = "admin";
+	private static final String PASSWORD = "19961121";
+	public static final String URL = "jdbc:mysql://"+ INSTANCE + ":" + PORT_NUM + "/" + DB_NAME
+			+ "?user=" + USERNAME + "&password=" + PASSWORD
+			+ "&autoReconnect=true&serverTimezone=UTC";
+	
+}
